65c5365156cfc76bf64f728f0cffdfb6889d82a2
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-10-04 16:47:52 +0600

    readme file update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,9 +9,9 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  pages: {
-    signIn: "/login",
-  },
+  // pages: {
+  //   signIn: "/login",
+  // },
 };
 
 export default NextAuth(authOptions);
